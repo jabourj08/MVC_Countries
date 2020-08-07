293b4fd316066107b396f8294e45c613c01ac6de
Josh <jabourjosh@gmail.com>
2020-08-06 21:55:47 -0400

    final commit

--- MVC_Countries/CountryController.cs
+++ MVC_Countries/CountryController.cs
@@ -19,6 +19,19 @@ namespace MVC_Countries
 
         public void WelcomeAction()
         {
+            List<Country> myCountries = new List<Country>()
+            {
+                new Country("United States of America", Continents.NorthAmerica, (new List<string>(){ "Red", "White", "Blue"})),
+                new Country("Italy", Continents.Europe, (new List<string>(){ "Green", "White", "Red"})),
+                new Country("Lebanon", Continents.Asia, (new List<string>(){ "Red", "White", "Green"})),
+                new Country("Belgium", Continents.Europe, (new List<string>(){ "Black", "Yellow", "Red"})),
+                new Country("Venezuela", Continents.SouthAmerica, (new List<string>(){ "Yellow", "Blue", "Red"})),
+                new Country("Madagascar", Continents.Africa, (new List<string>(){ "White", "Red", "Green"})),
+                new Country("Dominican Republic", Continents.NorthAmerica, (new List<string>(){ "Red", "White", "Blue"})),
+            };
+
+            CountryDb = myCountries;
+
             bool cont = true;
             while (cont)
             {
